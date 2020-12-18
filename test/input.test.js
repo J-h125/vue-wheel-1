e535e277cfa387810069c3cbd0d7cbfb62ae24a6
JH <473731589@qq.com>
2020-12-18 19:23:47 +0800

    让input支持 v-model双向绑定

--- test/input.test.js
+++ test/input.test.js
@@ -56,11 +56,14 @@ describe('Input', () => {
             ['change', 'input', 'focus', 'blur'].forEach((eventName) => {
                 vm = new Constructor({}).$mount()
                 const callback = sinon.fake();
-                vm.$on('change', callback)
-                let event = new Event('change');
+                vm.$on(eventName, callback)
+                let event = new Event(eventName);
+                Object.defineProperty(event,'target',{
+                    value:{value:'hi'},enumerable:true
+                })
                 let inputElement = vm.$el.querySelector('input')
                 inputElement.dispatchEvent(event)
-                expect(callback).to.have.been.calledWith(event)
+                expect(callback).to.have.been.calledWith('hi')
             })
         })
     })
