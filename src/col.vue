98088ca888731a928f9dfbc1a3c44cfabb7c43f3
JH <473731589@qq.com>
2020-12-20 19:47:58 +0800

    基本实现row和col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,33 +1,59 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[`span && col-${span}`,offset && `offset-${offset}`]"
+        :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+  >
+    <div style="border: 1px solid green; height: 100px">
     <slot></slot>
+    </div>
   </div>
 </template>
 
-<script >
+<script>
 export default {
-name: "Col",
-  props:{
-  span:{
-    type:[Number,String]
+  name: "Col",
+  props: {
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    },
+
   },
+  data(){
+    return{
+      gutter:0
+    }
+  },
+  created(){
+    console.log("row created");
+  },
+  mounted() {
+    console.log("row mounted")
+
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .col{
-    height: 100px;
-    width: 50%;
-    background: grey;
-    border: 1px solid red;
+.col {
 
-    $class-prefix: col-;
+  width: 50%;
 
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        width: ($n/24)*100% ;
-      }
+  //border: 1px solid red;
+
+  $class-prefix: col-;
+
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24)*100%;
     }
   }
+
+  @for $n from 1 through 24 {
+    &.offset-#{$n} {
+      margin-left: ($n/24)*100%;
+    }
+  }
+}
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}">
+
+
     <slot></slot>
+
   </div>
 </template>
 
 <script >
 export default {
-name: "Row"
+name: "Row",
+  props:{
+  gutter:{
+    type:[Number,String]
+  }
+  },
+    created(){
+      console.log('row created')
+    },
+    mounted() {
+      console.log('row mounted')
+      this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+      })
+    }
+
 }
 </script>
 
 <style lang="scss" scoped>
   .row{
     display: flex;
+
   }
 </style>
